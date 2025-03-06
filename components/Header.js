4c8cf840fb0ea64eda60083ad7c7c74d048721dd
adeliamj <adelia.miftakul.2205356@students.um.ac.id>
2025-03-06 15:35:52 +0700

    feat: homepage, eventpage, event item page

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
-                <Link href="/">
+                <Link href="/events">
                     DJ Events
                 </Link>
             </div>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,27 @@
 import Layout from "@/components/Layout"
+import EventItem from "@/components/EventItem"
+import {API_URL} from "@/config/index"
 
-export default function EventsPage() {
-    return (
-        <Layout title='Add New Event'>
-            <h1>My Events</h1>
-        </Layout>
-    )
+export default function EventsPage({events}) {
+  console.log(events)
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.lenght === 0 && <h3>No Events to Show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  )
 }
 
+
+export async function getStaticProps(){
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props:{events},
+    revalidate: 1,
+  }
+}
