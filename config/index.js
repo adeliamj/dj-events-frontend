e073b1d133c4e4cf6d8373ad9a4786bd3d01f856
adeliamj <adelia.miftakul.2205356@students.um.ac.id>
2025-03-14 13:15:38 +0700

    Update Environment Variables

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@
 // export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://exciting-strength-e9950013f2.strapiapp.com/'
 
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || ''
+export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://dj-events-frontend-sepia.vercel.app/'
 
 export const PER_PAGE = 4
